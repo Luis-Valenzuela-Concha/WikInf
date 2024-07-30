fc6091a0981a0cd2c59cd15c0551fe4af1a73db5
Francisco Cea <fcoceak@gmail.com>
2024-07-30 02:06:00 -0400

    feat: add old data (2011)

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,20 +1,45 @@
 export default {
-    useNextSeoProps() {
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - WikInf",
+    };
+  },
+  logo: <span>WikInf</span>,
+  gitTimestamp: (data) => (
+    <>
+      Última actualización{" "}
+      {new Date(data?.timestamp).toLocaleDateString("es-ES", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      })}
+    </>
+  ),
+  search: {
+    placeholder: "Buscar información...",
+    error: "Ocurrió un error inesperado",
+    emptyResult: () => (
+      <span className="nx-block nx-select-none nx-p-8 nx-text-center nx-text-sm nx-text-gray-400">
+        No se encontraron resultados
+      </span>
+    ),
+  },
+  editLink: {
+    text: "Constribuye a esta página →",
+  },
+  feedback: {
+    content: null,
+  },
+  toc: {
+    title: "En esta página",
+  },
+  themeSwitch: {
+    useOptions() {
       return {
-        titleTemplate: 'WikInf - %s'
-      }
+        light: "Claro",
+        dark: "Oscuro",
+        system: "Sistema",
+      };
     },
-    logo: <span>WikInf</span>,
-    gitTimestamp: (data) => <>Última actualización {new Date(data?.timestamp).toLocaleDateString('es-ES', { day: 'numeric', month: 'long', year: 'numeric' })}</>,
-    search: {
-      placeholder: 'Buscar información...',
-      error: 'Ocurrió un error inesperado',
-      emptyResult: () => <span className="nx-block nx-select-none nx-p-8 nx-text-center nx-text-sm nx-text-gray-400">No se encontraron resultados</span>,
-    },
-    editLink: {
-      text: "Constribuye a esta página"
-    },
-    feedback: {
-      content: null
-    }
-  }
+  },
+};
